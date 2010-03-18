77c5e5c99f2050c69a6c7030ae2a66a769bca053
OISHI Eisuke <e-oishi@ark.(none)>
2010-03-18 15:23:18 +0900

    upgrade version

--- lib/DBIx/ObjectMapper.pm
+++ lib/DBIx/ObjectMapper.pm
@@ -2,7 +2,7 @@ package DBIx::ObjectMapper;
 use strict;
 use warnings;
 use 5.008_001;
-our $VERSION = '0.1001';
+our $VERSION = '0.3001';
 
 use Carp::Clan qw/^DBIx::ObjectMapper/;
 use Params::Validate qw(:all);
