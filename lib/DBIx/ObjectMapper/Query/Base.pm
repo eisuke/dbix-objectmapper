6623a01dd6c7d92aa7d16fad90830b1d3d7347cc
William Schroeder <wschroed@genome.wustl.edu>
2011-07-27 23:24:38 -0500

    Implemented query cloning.
    By default, OM supports mutable query building.  clone() allows us to extend
    queries without affecting the original queries.  See 09_query/000_basic.t for
    a specific example of how this works.

--- lib/DBIx/ObjectMapper/Query/Base.pm
+++ lib/DBIx/ObjectMapper/Query/Base.pm
@@ -1,7 +1,7 @@
 package DBIx::ObjectMapper::Query::Base;
 use strict;
 use warnings;
-use Scalar::Util qw(weaken);
+use Scalar::Util qw(weaken blessed);
 use Carp::Clan qw/^DBIx::ObjectMapper/;
 
 sub new {
@@ -35,6 +35,57 @@ sub execute { die "Abstract Method" }
 sub as_sql {
     my $self = shift;
     return $self->builder->as_sql(@_);
+}
+
+# By default, OM supports mutable query building.  clone() allows us to extend
+# queries without affecting the original queries.
+sub clone {
+    my $self = shift;
+    my ($clone_hash, $clone_array, $clone_element);
+
+    $clone_hash = sub {
+        my $hash = shift;
+        return {
+            map {
+                $_ => $clone_element->($hash->{$_})
+            } keys %$hash
+        };
+    };
+
+    $clone_array = sub {
+        my $array = shift;
+        return [
+            map {
+                $clone_element->($_)
+            } @$array
+        ];
+    };
+
+    $clone_element = sub {
+        my $element = shift;
+
+        if (!ref $element || ref $element eq 'CODE') {
+            return $element;
+        }
+        elsif (blessed $element && $element->can('clone')) {
+            return $element->clone;
+        }
+        elsif (blessed $element) {
+            return $element;
+        }
+        elsif (ref $element eq 'HASH') {
+            return $clone_hash->($element);
+        }
+        elsif (ref $element eq 'ARRAY') {
+            return $clone_array->($element);
+        }
+        else {
+            use Data::Dumper;
+            die "I do not know how to clone: " . Dumper($element);
+        }
+    };
+
+    return bless($clone_hash->($self), blessed $self);
 }
 
 sub DESTROY {
