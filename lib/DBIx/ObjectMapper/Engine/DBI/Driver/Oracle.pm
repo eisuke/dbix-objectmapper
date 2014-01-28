cbf55658b9557c194201314aa01c67267f1f41d6
Shawn Leonard <sleonard@genome.wustl.edu>
2014-01-28 12:46:48 -0600

    DBD::Oracle overrides to remove deprecated RULE* hint until we can upgrade to DBD::Oracle 1.68

--- lib/DBIx/ObjectMapper/Engine/DBI/Driver/Oracle.pm
+++ lib/DBIx/ObjectMapper/Engine/DBI/Driver/Oracle.pm
@@ -40,7 +40,8 @@ sub get_primary_key {
     my ($self, $dbh, $table) = @_;
     if (!$self->{_cache}->{_oracle}->{primary_keys}->{$table}) {
          $self->{_cache}->{_oracle}->{primary_keys}->{$table} =
-            +[keys %{$dbh->primary_key_info('', $self->db_schema, $table)->fetchall_hashref('COLUMN_NAME')}];
+            #+[keys %{$dbh->primary_key_info('', $self->db_schema, $table)->fetchall_hashref('COLUMN_NAME')}];
+            +[keys %{$self->_primary_key_info($dbh, '', $self->db_schema, $table)->fetchall_hashref('COLUMN_NAME')}];
     }
     return @{$self->{_cache}->{_oracle}->{primary_keys}->{$table}};
 }
@@ -82,7 +83,8 @@ sub get_table_fk_info {
     my ($self, $dbh, $table) = @_;
 
     if (!$self->{_cache}->{_oracle}->{foreign_keys}->{$table}) {
-        my $sth = $dbh->foreign_key_info(undef, undef, undef, '', $self->db_schema, $table);
+        #my $sth = $dbh->foreign_key_info(undef, undef, undef, '', $self->db_schema, $table);
+        my $sth = $self->_foreign_key_info($dbh,undef, undef, undef, '', $self->db_schema, $table);
         my %constraints = ();
 
         while (my $row = $sth->fetchrow_hashref) {
@@ -184,6 +186,101 @@ sub type_map {
     my $type  = shift;
     $type =~ s/\(.*$//;
     return $class->_type_map_data->{$type};
+}
+
+###### horrid DBD::Oracle override below 
+###### REMOVE WHEN PERL IS UPGRADED!!!!!
+###### code base from DBD::Oracle v1.68
+
+sub _primary_key_info {
+    my($self, $dbh, $catalog, $schema, $table) = @_;
+    if (ref $catalog eq 'HASH') {
+        ($schema, $table) = @$catalog{'TABLE_SCHEM','TABLE_NAME'};
+        $catalog = undef;
+    }
+    my $SQL = <<'SQL';
+SELECT *
+  FROM
+(
+  SELECT /*+ CHOOSE */
+         NULL              TABLE_CAT
+       , c.OWNER           TABLE_SCHEM
+       , c.TABLE_NAME      TABLE_NAME
+       , c.COLUMN_NAME     COLUMN_NAME
+       , c.POSITION        KEY_SEQ
+       , c.CONSTRAINT_NAME PK_NAME
+    FROM ALL_CONSTRAINTS   p
+       , ALL_CONS_COLUMNS  c
+   WHERE p.OWNER           = c.OWNER
+     AND p.TABLE_NAME      = c.TABLE_NAME
+     AND p.CONSTRAINT_NAME = c.CONSTRAINT_NAME
+     AND p.CONSTRAINT_TYPE = 'P'
+)
+ WHERE TABLE_SCHEM = ?
+   AND TABLE_NAME  = ?
+ ORDER BY TABLE_SCHEM, TABLE_NAME, KEY_SEQ
+SQL
+#warn "@_\n$Sql ($schema, $table)";
+    my $sth = $dbh->prepare($SQL) or return undef;
+    $sth->execute($schema, $table) or return undef;
+    $sth;
+}
+
+sub _foreign_key_info {
+    my $self = shift;
+    my $dbh  = shift;
+    my $attr = ( ref $_[0] eq 'HASH') ? $_[0] : {
+        'UK_TABLE_SCHEM' => $_[1],'UK_TABLE_NAME ' => $_[2]
+            ,'FK_TABLE_SCHEM' => $_[4],'FK_TABLE_NAME ' => $_[5] };
+    my $SQL = <<'SQL';  # XXX: DEFERABILITY
+SELECT *
+  FROM
+(
+  SELECT /*+ CHOOSE */
+         to_char( NULL )    UK_TABLE_CAT
+       , uk.OWNER           UK_TABLE_SCHEM
+       , uk.TABLE_NAME      UK_TABLE_NAME
+       , uc.COLUMN_NAME     UK_COLUMN_NAME
+       , to_char( NULL )    FK_TABLE_CAT
+       , fk.OWNER           FK_TABLE_SCHEM
+       , fk.TABLE_NAME      FK_TABLE_NAME
+       , fc.COLUMN_NAME     FK_COLUMN_NAME
+       , uc.POSITION        ORDINAL_POSITION
+       , 3                  UPDATE_RULE
+       , decode( fk.DELETE_RULE, 'CASCADE', 0, 'RESTRICT', 1, 'SET NULL', 2, 'NO ACTION', 3, 'SET DEFAULT', 4 )
+                            DELETE_RULE
+       , fk.CONSTRAINT_NAME FK_NAME
+       , uk.CONSTRAINT_NAME UK_NAME
+       , to_char( NULL )    DEFERABILITY
+       , decode( uk.CONSTRAINT_TYPE, 'P', 'PRIMARY', 'U', 'UNIQUE')
+                            UNIQUE_OR_PRIMARY
+    FROM ALL_CONSTRAINTS    uk
+       , ALL_CONS_COLUMNS   uc
+       , ALL_CONSTRAINTS    fk
+       , ALL_CONS_COLUMNS   fc
+   WHERE uk.OWNER            = uc.OWNER
+     AND uk.CONSTRAINT_NAME  = uc.CONSTRAINT_NAME
+     AND fk.OWNER            = fc.OWNER
+     AND fk.CONSTRAINT_NAME  = fc.CONSTRAINT_NAME
+     AND uk.CONSTRAINT_TYPE IN ('P','U')
+     AND fk.CONSTRAINT_TYPE  = 'R'
+     AND uk.CONSTRAINT_NAME  = fk.R_CONSTRAINT_NAME
+     AND uk.OWNER            = fk.R_OWNER
+     AND uc.POSITION         = fc.POSITION
+)
+ WHERE 1              = 1
+SQL
+    my @BindVals = ();
+    while ( my ( $k, $v ) = each %$attr ) {
+        if ( $v ) {
+            $SQL .= "   AND $k = ?\n";
+            push @BindVals, $v;
+        }
+    }
+    $SQL .= " ORDER BY UK_TABLE_SCHEM, UK_TABLE_NAME, FK_TABLE_SCHEM, FK_TABLE_NAME, ORDINAL_POSITION\n";
+    my $sth = $dbh->prepare( $SQL ) or return undef;
+    $sth->execute( @BindVals ) or return undef;
+    $sth;
 }
 
 1;
